608665eafe5b175238f5550c7af31967ff7600e8
Victor Oketch Sabare <sabarevictor@gmail.com>
2022-09-18 16:45:24 +0300

    Update AuthorLayout.js

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -26,8 +26,8 @@ export default function AuthorLayout({ children, frontMatter }) {
                 className="rounded-full"
               />
             </div>
-            <h3 className="py-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
-            <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
+            <h3 className="py-5 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
+            <div className="py-5 text-gray-500 dark:text-gray-400">{occupation}</div>
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
             <div className="flex space-x-3 py-6 px-4 md:py-0 md:px-6">
               <SocialIcon kind="mail" href={`mailto:${email}`} size={5} />
