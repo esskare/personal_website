e6f5dd77345118900d1b82f9b0583c232255c737
Victor Oketch Sabare <sabarevictor@gmail.com>
2022-09-18 16:26:29 +0300

    finishes

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
+  title: "Ess Karee's Blog",
+  author: 'Esther Wangari',
   headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'A blog highlighting my Computer Science journey.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
