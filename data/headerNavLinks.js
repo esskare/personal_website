789031cadc754564a23b2f8bc2006638c1261666
Victor Oketch Sabare <sabarevictor@gmail.com>
2022-09-18 14:02:35 +0300

    heading

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,8 @@
 const headerNavLinks = [
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  { href: '/', title: 'home' },
+  { href: '/blog', title: 'blog' },
+  // { href: '/tags', title: 'tags' },
+  { href: '/projects', title: 'projects' },
   { href: '/about', title: 'About' },
 ]
 
